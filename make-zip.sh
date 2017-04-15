f40eec7cf3dab28775558be19fa323c72c77fe99
Andrew Black <elystus@gmail.com>
2017-04-15 14:20:38 -0700

    Adding zip creator script

---
+++ make-zip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -d "src/venv" ]; then
+    if [ -d "makezip" ]; then
+        rm -rf makezip
+    fi
+
+    if [ -d jawbone.zip ]; then
+        rm -rf jawbone.zip
+    fi
+
+    mkdir makezip
+    cp src/*.py makezip/
+    cp -R src/alexa makezip/alexa
+    cp -R src/jawbone makezip/jawbone
+    cp -R src/venv/lib/python2.7/site-packages/* makezip/
+    cd makezip
+    zip -r ../jawbone.zip *
+    cd ../
+    rm -rf makezip
+fi
